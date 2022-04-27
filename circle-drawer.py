15c291592f6322b927db0ce05f0bd73e991d36fe
Miles Conrad <72412676+milesconrad@users.noreply.github.com>
2022-04-27 00:16:20 -0400

    Update circle-drawer.py

--- circle-drawer.py
+++ circle-drawer.py
@@ -5,13 +5,13 @@ from time import sleep
 sleep(3)
 mouse = Controller()
 
-for i in range(360 * 3 + 40):
-    x = 960.5 + 400 * cos(radians(i / 3))
-    y = 520.5 + 400 * sin(radians(i / 3))
+for i in range(360 + 10):
+    x = 960.5 + 400 * cos(radians(i))
+    y = 520.5 + 400 * sin(radians(i))
 
     mouse.position = (x, y)
     if i == 0:
         mouse.press(Button.left)
-    sleep(0.003)
+    sleep(0.001)
 
 mouse.release(Button.left)
